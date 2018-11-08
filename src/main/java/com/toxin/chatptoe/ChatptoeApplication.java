a9a6290ae2907ce8db5c85b5036e4329d000c709
toxin <yuri.belousov.1998@gmail.com>
2018-11-08 16:13:21 +0300

    init

---
+++ src/main/java/com/toxin/chatptoe/ChatptoeApplication.java
@@ -0,0 +1,12 @@
+package com.toxin.chatptoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatptoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatptoeApplication.class, args);
+    }
+}
